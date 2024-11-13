e6ab22433a8b90ff31df6644ab71e92d0c860486
OfirYC <ofir@yieldchain.io>
2024-11-13 16:45:27 +0200

    Limit adrenalin taggers

--- plugins/TagEveryone.ts
+++ plugins/TagEveryone.ts
@@ -2,6 +2,16 @@ import { proto, AnyMessageContent } from "@whiskeysockets/baileys";
 import { GetText, Plugin, SendMessage, WAWebsocket } from "../types";
 import { containsTrigger } from "../utils";
 
+const ADRENALIN_GC_ID = "120363024375490421@g.us";
+
+const ADRENALIN_ADMINS_IDS = new Set([
+  "972532200486@s.whatsapp.net",
+  "972525300584@s.whatsapp.net",
+  "972502228097@s.whatsapp.net",
+  "972583656560@s.whatsapp.net",
+  "972559727617@s.whatsapp.net",
+]);
+
 export class TagEveryone extends Plugin {
   #membersLimit: number;
   #triggers;
@@ -23,8 +33,23 @@ export class TagEveryone extends Plugin {
       return;
     }
 
+    const sender = key.participant;
+
+    if (!sender) {
+      console.error("NO SENDER", key, message);
+      return;
+    }
+
     try {
       const grp = await this.socket.groupMetadata(key.remoteJid);
+
+      if (!key.fromMe && grp.id == ADRENALIN_GC_ID && !ADRENALIN_ADMINS_IDS.has(sender)) {
+        console.error("Sender unallowed to tag everyone!");
+        return;
+      } else {
+        console.log("SER SER SER")
+      }
+
       const members = grp.participants;
 
       const mentions: string[] = [];
